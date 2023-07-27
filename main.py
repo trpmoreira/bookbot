791f0a02478e2b33e1be6f456569bbc2d32443b8
trpmoreira <trpmoreira@gmail.com>
2023-07-27 22:51:55 +0100

    added report printing function

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-with open("books/frankenstein.txt") as f:
+book = "books/frankenstein.txt"
+
+with open(book) as f:
     file_contents = f.read()
 
 
 def countWords(text):
     words = text.split()
-    print(f"This book has {len(words)} words in it.")
+    return len(words)
 
 
 def countChars(text):
@@ -12,12 +14,27 @@ def countChars(text):
     chars = list(text.lower())
 
     for char in chars:
-        if char not in result:
-            result[char] = 1
-        else:
-            result[char] += 1
-    print(result)
+        if char.isalpha() == True:
+            if char not in result:
+                result[char] = 1
+            else:
+                result[char] += 1
+    new_results = list(result.items())
+# want to sort by the second item in the tuple
+    new_results.sort(key=lambda x: x[1], reverse=True)
+    return new_results
 
 
-countWords(file_contents)
-countChars(file_contents)
+def report(text):
+    words = countWords(text)
+    char = countChars(text)
+
+    print(f"--- Begin report of {book} ---")
+    print(f"The document contains {words} words")
+    print("")
+    for item in char:
+        print(f"The '{item[0]}' character was found {item[1]} times")
+    print(f"--- End report ---")
+
+
+report(file_contents)
